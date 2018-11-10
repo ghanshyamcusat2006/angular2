5cb855a97207865f22c61018ad7d78399198e4d8
ghanshyamcusat2006 <ghanshyam.cusat2006@gmail.com>
2018-11-10 20:53:14 +0530

    angular attribute binding code commit

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'employee',
+  templateUrl: `app/employee/employee.component.html`,
+  styleUrls: [`app/employee/employee.component.css`]
+})
+export class EmployeeComponent  { 
+firstName:string = 'Ghanshyam';
+lastName:string = 'Kumar';
+salary:number = '5000';
+department:string = 'Training';
+imgPath:string="https://www.tutorialspoint.com/images/logo.png";
+colspan:number=2;
+
+empName(): string{
+     return this.firstName + ' ' + this.lastName;
+   }
+ }

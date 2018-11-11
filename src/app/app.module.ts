a530e09893ab7ecbf110c87954b969f388989612
ghanshyamcusat2006 <ghanshyam.cusat2006@gmail.com>
2018-11-11 19:40:24 +0530

    angular routing code commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent }  from './app.component';
+import { HomeComponent }  from './home/home.component';
+import { EmployeeComponent }  from './employee/employee.component';
+import { PnfComponent }  from './misc/pnf.component';
+
+const appRoutes: Routes = [
+    {path: 'home', component: HomeComponent},
+	{path: 'employee', component: EmployeeComponent},
+	{path: 'employee/:id', component: EmployeeComponent},
+	{path: '', redirectTo: '/home',pathMatch: 'full'},
+	{path: '**', component: PnfComponent}
+];
+
+@NgModule({
+  imports:      [ BrowserModule, RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent, HomeComponent, EmployeeComponent, PnfComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  templateUrl: `app/home/home.component.html`
+})
+export class HomeComponent  { 
+ }

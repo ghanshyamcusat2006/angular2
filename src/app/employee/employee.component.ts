128f20cced08794b8f24d318b42d7fbecbb2e8ca
ghanshyamcusat2006 <ghanshyam.cusat2006@gmail.com>
2018-11-11 20:29:20 +0530

    angular navigation code commit

---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: `app/employee/employee.component.html`
+})
+export class EmployeeComponent  { }
---
+++ src/app/misc/pnf.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  templateUrl: `app/misc/pnf.component.html`
+})
+export class PnfComponent  { }

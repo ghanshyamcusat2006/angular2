1f66a1b8f485aae8b64ce151f1b703e927d5693c
ghanshyamcusat2006 <ghanshyam.cusat2006@gmail.com>
2018-11-11 14:45:58 +0530

    angular two way binding code commit

---
+++ src/app/sample/sample.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sample',
+  templateUrl: `app/sample/sample.component.html`,
+})
+export class SampleComponent  { 
+val:string = 'Ghanshyam';
+va:string = 'shyam';
+
+ }
